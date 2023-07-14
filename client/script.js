8493a9d152b75072ec5611e37613962c83b27079
Morgan Bonar <morganbonar@Morgans-MBP.lan>
2023-07-13 20:40:13 -0700

    update the server url

--- client/script.js
+++ client/script.js
@@ -66,7 +66,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bots respose
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-openai-6pjd.onrender.com', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
